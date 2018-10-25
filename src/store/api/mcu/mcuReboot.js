8b983c78267ff61f7ad2865c69cd8e741f636bac
Aleksander Barszczewski <alek.barszczewski@gmail.com>
2018-10-25 15:26:46 +0200

    mcu api

---
+++ src/store/api/mcu/mcuReboot.js
@@ -0,0 +1,7 @@
+const { exec } = require('child_process')
+
+module.exports = ({ define }) => {
+  define('reboot', async (payload, { knex, errors, utils }) => {
+    exec('shutdown -r now')
+  })
+}
---
+++ src/store/api/mcu/mcuShutdown.js
@@ -0,0 +1,7 @@
+const { exec } = require('child_process')
+
+module.exports = ({ define }) => {
+  define('shutdown', async (payload, { knex, errors, utils }) => {
+    exec('shutdown now')
+  })
+}
