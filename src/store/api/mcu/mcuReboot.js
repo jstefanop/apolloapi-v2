f35c10e094a6494075072657d2d68d52f1eff69c
Michele Marcucci <michele@befree.it>
2018-11-20 18:29:07 +0100

    Installation script TODO

--- src/store/api/mcu/mcuReboot.js
+++ src/store/api/mcu/mcuReboot.js
@@ -2,7 +2,7 @@ const { exec } = require('child_process')
 
 module.exports = ({ define }) => {
   define('reboot', async (payload, { knex, errors, utils }) => {
-    if (process.env.NODE_ENV === 'production') return exec('sudo shutdown -r now');
+    if (process.env.NODE_ENV === 'production') return exec('/etc/system_reboot.sh');
     return;
   }, {
     auth: true
--- src/store/api/mcu/mcuShutdown.js
+++ src/store/api/mcu/mcuShutdown.js
@@ -2,7 +2,7 @@ const { exec } = require('child_process')
 
 module.exports = ({ define }) => {
   define('shutdown', async (payload, { knex, errors, utils }) => {
-    if (process.env.NODE_ENV === 'production') return exec('sudo shutdown now')
+    if (process.env.NODE_ENV === 'production') return exec('/etc/system_shutdown.sh')
     return;
   }, {
     auth: true
