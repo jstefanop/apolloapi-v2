d6c0827c9b577646e835da4f5f064579ee074868
Michele Marcucci <michele@befree.it>
2018-11-27 12:51:57 +0100

    Frontend inside build folder

--- src/app/app.js
+++ src/app/app.js
@@ -2,7 +2,7 @@ const path = require('path')
 const express = require('express')
 const config = require('config')
 const graphqlApp = require('./graphqlApp')
-const buildPath = path.join(__dirname, '../../apolloui/build');
+const buildPath = path.join(__dirname, '../../build');
 
 const app = express()
 
