51b645a960041199224333c889ce630da4a05c43
Alex Thomas <alex@areios.io>
2020-03-05 15:37:05 -0500

    Clean up minerClient.js file

--- src/app/minerClient.js
+++ src/app/minerClient.js
@@ -1,9 +1,9 @@
 'use strict';
- 
+
 const net = require('net');
 const PORT = 4028;
 const HOST = '127.0.0.1';
- 
+
 class Client {
  constructor(port, address) {
   this.socket = new net.Socket();
@@ -11,13 +11,13 @@ class Client {
   this.port = port || PORT;
   this.init();
  }
- 
+
  init() {
   var client = this;
   client.socket.connect(client.port, client.address, () => {
    console.log(`Client connected to: ${client.address} :  ${client.port}`);
   });
- 
+
   client.socket.on('close', () => {
    console.log('Client closed');
   });
