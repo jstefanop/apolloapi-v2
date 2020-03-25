961226249031208dffc642db1cbba5ec1ebcf03d
Alex Thomas <alex@areios.io>
2020-03-24 23:43:55 -0400

    Update build files

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.62cb1f8f6045e8473980532d3c34b5a5.js"
+  "/precache-manifest.c6186d1eb406e039b582ba7c1516fdca.js"
 );
 
 workbox.clientsClaim();
