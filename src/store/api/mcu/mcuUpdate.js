4c6376f7b8865672fc77e1541274c86f47954d66
Michele Marcucci <michele@befree.it>
2021-04-29 10:35:54 +0200

    Update ready

--- src/store/api/mcu/mcuUpdate.js
+++ src/store/api/mcu/mcuUpdate.js
@@ -1,12 +1,25 @@
 const { join } = require('path')
-const { exec } = require('child_process')
+const { spawn } = require('child_process')
 
 module.exports = ({ define }) => {
   define('update', async (payload, { knex, errors, utils }) => {
-  	const updateScript = join(__dirname, '..', '..', '..', '..', 'backend', 'update')
-    if (process.env.NODE_ENV === 'production') return exec(`bash ${updateScript}`)
-    else console.log(updateScript)
-    return;
+  	let scriptName = 'update.fake';
+  	if (process.env.NODE_ENV === 'production') scriptName = 'update';
+  	const updateScript = join(__dirname, '..', '..', '..', '..', 'backend', scriptName)
+    const cmd = spawn('bash',  [updateScript])
+
+   	cmd.stdout.on('data', (data) => {
+	  console.log(`stdout: ${data}`);
+	});
+
+	cmd.stderr.on('data', (data) => {
+	  console.error(`stderr: ${data}`);
+	});
+
+	cmd.on('close', (code) => {
+	  console.log(`child process exited with code ${code}`);
+	  return;
+	});
   }, {
     auth: true
   })
