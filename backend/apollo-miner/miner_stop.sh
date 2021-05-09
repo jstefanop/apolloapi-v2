bdd489a1ca7ff87e71562e61658d015893bda7c1
John <john@future-apps.net>
2021-05-08 20:24:29 -0400

    Upload Release Binaries

--- backend/apollo-miner/miner_stop.sh
+++ backend/apollo-miner/miner_stop.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
+#quit all mining proccesses
+for scr in $(screen -ls | awk '{print $1}'); do screen -S $scr -X quit; done
+
+#reset internal hashboard
+gpio write 0 0
+sleep .5
+gpio write 0 1
+
 echo "Stopped"
