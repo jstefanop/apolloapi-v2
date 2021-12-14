cae1a92d506ce5e3a0ab043fd59d939583da8ffd
Michele Marcucci <michele@befree.it>
2021-12-14 09:15:30 +0100

    Format script call

--- src/store/api/node/nodeFormat.js
+++ src/store/api/node/nodeFormat.js
@@ -4,26 +4,24 @@ const axios = require('axios')
 
 module.exports = ({ define }) => {
   define('format', async (payload, { knex, errors, utils }) => {
-    await formatDisk();
-  }, {
-    auth: true
-  })
-}
-
-function formatDisk () {
-  return new Promise((resolve, reject) => {
     const scriptName = (process.env.NODE_ENV === 'production') ? 'format_node_disk' : 'format_node_disk_fake'
     const scriptPath = join(__dirname, '..', '..', '..', '..', 'backend', scriptName)
-    exec(`sudo ${scriptPath}`, {}, (err, stdout) => {
-      if (err) {
-        reject(err)
-      } else {
-        try {
-          resolve();
-        } catch (err) {
-          reject(err)
-        }
-      }
-    })
+    const cmd = spawn('sudo',  ['bash', scriptPath])
+
+    cmd.stdout.on('data', (data) => {
+      console.log(`stdout: ${data}`);
+    });
+
+    cmd.stderr.on('data', (data) => {
+      console.error(`stderr: ${data}`);
+      reject(err);
+    });
+
+    cmd.on('close', (code) => {
+      console.log(`child process exited with code ${code}`);
+      resolve();
+    });
+  }, {
+    auth: true
   })
 }
