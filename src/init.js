dae42bbe88eddba30a303f5cbd440f86eea33ae9
Michele Marcucci <michele@befree.it>
2022-07-20 14:22:18 +0200

    Fixed Bitcoin RPC password generator when initialSetup is not done (updating)

--- src/init.js
+++ src/init.js
@@ -1,12 +1,15 @@
-const { writeFileSync, existsSync } = require('fs')
+const { writeFileSync, readFileSync, existsSync } = require('fs')
 const { join } = require('path')
 const crypto = require('crypto')
+const generator = require('generate-password')
+const utils = require('./utils')
+const { knex } = require('./db')
 
 initEnvFile()
 runMigrations()
 .then(startServer)
 
-function initEnvFile () {
+async function initEnvFile () {
   const envPath = join(__dirname, '..', '.env')
   const envExists = existsSync(envPath)
   if (!envExists) {
@@ -23,11 +26,27 @@ function initEnvFile () {
       .map(({ name, value }) => `${name}=${value}`)
       .join('\n') + '\n'
     writeFileSync(envPath, envFile)
+  } else {
+    const envFile = readFileSync(envPath)
+    let envString = envFile.toString();
+    if (!envString.match(/BITCOIND_PASSWORD=.*/)) {
+      console.log('Generating Bitcoin RPC password')
+
+      const rpcPassword = generator.generate({
+        length: 12,
+        numbers: true
+      })
+
+      await knex('settings').update({
+        node_rpc_password: rpcPassword
+      })
+
+      await utils.auth.changeNodeRpcPassword(rpcPassword)
+    }
   }
 }
 
 async function runMigrations () {
-  const { knex } = require('./db')
   try {
     const resp = await knex.migrate.latest()
   } catch (err) {
