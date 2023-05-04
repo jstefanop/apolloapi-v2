bdef32f0cebbf3bdf27f96877cee1e864b573c7c
Michele Marcucci <michele.marcucci@simplicissimus.it>
2023-05-04 08:41:58 +0200

    Fix wifi disconnect

--- src/store/api/mcu/mcuWifiDisconnect.js
+++ src/store/api/mcu/mcuWifiDisconnect.js
@@ -11,7 +11,7 @@ module.exports = ({ define }) => {
 
 function wifiDisconnect(ssid, passphrase, errors) {
   return new Promise((resolve, reject) => {
-    let command = 'for i in $(nmcli -t -f uuid c show); do nmcli c delete $i; done';
+    let command = 'for i in $(nmcli -t c show|grep wlan); do nmcli c delete `echo $i|cut -d":" -f2`; done';
     if (process.env.NODE_ENV !== 'production') command = 'sleep 2 && echo true';
 
     exec(command, {}, (err, stdout) => {
