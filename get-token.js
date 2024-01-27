feca08e75ce03a3d1fbc220251fc8666fdcedb50
Michele Marcucci <michele.marcucci@simplicissimus.it>
2024-01-27 10:16:00 +0100

    WIP install-v2

--- get-token.js
+++
@@ -1,9 +0,0 @@
-const jwt = require('jsonwebtoken')
-const config = require('config')
-
-const accessToken = jwt.sign({}, config.get('server.secret'), {
-  subject: 'apollouser',
-  audience: 'auth'
-})
-
-console.log(accessToken)
