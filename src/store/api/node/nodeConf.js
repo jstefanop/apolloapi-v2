e1b6389a9c4e2e329f1252eb77c0b4564263818a
Michele Marcucci <michele.marcucci@simplicissimus.it>
2024-01-30 15:14:51 +0100

    Bitcoin and nodeStats changes

--- src/store/api/node/nodeConf.js
+++ src/store/api/node/nodeConf.js
@@ -4,6 +4,6 @@ module.exports = ({ define }) => {
   define('conf', async (payload, { knex, errors, utils }) => {
     const bitcoinConf = await fs.promises.readFile('/opt/apolloapi/backend/node/bitcoin.conf');
 
-    return { bitcoinConf: bitcoinConf.toString() };
+    return { bitcoinConf: bitcoinConf ? bitcoinConf.toString() : ''};
   })
 }
--- src/store/api/node/nodeStats.js
+++ src/store/api/node/nodeStats.js
@@ -1,176 +1,148 @@
- const { join } = require('path')
-const { exec } = require('child_process')
-
-const bitcoin = require('litecoin');
+const { promisify } = require('util');
+const bitcoin = require('bitcoin');
 
 module.exports = ({ define }) => {
-  define('stats', async (payload, { knex, errors, utils }) => {
-    try {
-      const [ settings ] = await knex('settings').select(['node_rpc_password as nodeRpcPassword'])
-      const bitcoinClient = new bitcoin.Client({
-        host: process.env.BITCOIN_NODE_HOST || '127.0.0.1',
-        port: process.env.BITCOIN_NODE_PORT || 8332,
-        user: process.env.BITCOIN_NODE_USER || 'futurebit',
-        pass: process.env.BITCOIN_NODE_PASS || settings.nodeRpcPassword,
-        timeout: 30000,
-        ssl: false
-      });
+  define(
+    'stats',
+    async (payload, { knex, errors, utils }) => {
+      try {
+        const settings = await knex('settings').select([
+          'node_rpc_password as nodeRpcPassword',
+        ]);
+        const bitcoinClient = createBitcoinClient(settings);
 
-      const unrefinedStats = await getNodeStats(bitcoinClient);
+        const unrefinedStats = await getNodeStats(bitcoinClient);
 
-      // At this point, no error present
+        const blockchainInfo = await formatBlockchainInfo(
+          bitcoinClient,
+          unrefinedStats[0]
+        );
+        const miningInfo = formatMiningInfo(unrefinedStats[2]);
+        const peerInfo = formatPeerInfo(unrefinedStats[3]);
+        const networkInfo = formatNetworkInfo(unrefinedStats[4]);
 
-      const unrefinedBlockchainInfo = unrefinedStats[0];
+        const stats = {
+          blockchainInfo,
+          connectionCount: unrefinedStats[1],
+          miningInfo,
+          peerInfo,
+          networkInfo,
+          error: null,
+          timestamp: new Date().toISOString(),
+        };
 
-      // Convert sizeOnDisk to String because number too large
-      const blockchainInfo = {
-        blocks: unrefinedBlockchainInfo.blocks,
-        blockTime: unrefinedBlockchainInfo.blockTime,
-        headers: unrefinedBlockchainInfo.headers,
-        sizeOnDisk: unrefinedBlockchainInfo.size_on_disk.toString()
-      };
+        return { stats };
+      } catch (error) {
+        const stats = {
+          error: {
+            code: error.code,
+            message: error.errno || error.message,
+          },
+          timestamp: new Date().toISOString(),
+        };
 
-      // Strip miningInfo of unnecessary properties
-      const unrefinedMiningInfo = unrefinedStats[2];
-      const miningInfo = {
-        difficulty: unrefinedMiningInfo.difficulty,
-        networkhashps: unrefinedMiningInfo.networkhashps
-      };
-
-      // Strip peerInfo of unnecessary properties
-      const unrefinedPeerInfo = unrefinedStats[3];
-      const peerInfo = unrefinedPeerInfo.map(({ addr, subver }) => ({
-        addr,
-        subver
-      }));
-
-      // Strip networkInfo of unnecessary properties
-      const unrefinedNetworkInfo = unrefinedStats[4];
-      const networkInfo = {
-        version: unrefinedNetworkInfo.version,
-        subversion: unrefinedNetworkInfo.subversion,
-        localaddresses: unrefinedNetworkInfo.localaddresses,
-      };
-
-      // Convert unrefinedStats to object
-      const stats = {
-        blockchainInfo: blockchainInfo,
-        connectionCount: unrefinedStats[1],
-        miningInfo: miningInfo,
-        peerInfo: peerInfo,
-        networkInfo: networkInfo,
-        error: null
-      };
-
-      stats.timestamp = new Date().toISOString();
-
-      return { stats }
-    } catch (error) {
-      // Use errno for API not available, and use description for API loading
-      const stats = {
-        error: {
-          code: error.code,
-          message: error.errno || error.message
-        },
-        timestamp: new Date().toISOString()
-      };
-
-      return { stats };
+        return { stats };
+      }
+    },
+    {
+      auth: true,
     }
-  }, {
-    auth: true
-  })
-}
+  );
+};
 
-function getNodeStats (bitcoinClient) {
-  const getBlockchainInfoPromise = new Promise((resolve, reject) => {
-    bitcoinClient.getBlockchainInfo((error, blockchainInfo) => {
-      if (error) {
-        reject(error)
+const createBitcoinClient = (settings) => {
+  try {
+    const bitcoinClient = new bitcoin.Client({
+      host: process.env.BITCOIN_NODE_HOST || '127.0.0.1',
+      port: process.env.BITCOIN_NODE_PORT || 8332,
+      user: process.env.BITCOIN_NODE_USER || 'futurebit',
+      pass: process.env.BITCOIN_NODE_PASS || settings.nodeRpcPassword,
+      timeout: 30000,
+    });
+
+    return bitcoinClient;
+  } catch (error) {
+    throw error;
+  }
+};
+
+const formatBlockchainInfo = async (bitcoinClient, unrefinedBlockchainInfo) => {
+  try {
+    const bestBlockHash = unrefinedBlockchainInfo.bestblockhash;
+    const block = await getBitcoinBlock(bitcoinClient, bestBlockHash);
+
+    unrefinedBlockchainInfo.blockTime = block.time;
+
+    return {
+      blocks: unrefinedBlockchainInfo.blocks,
+      blockTime: unrefinedBlockchainInfo.blockTime,
+      headers: unrefinedBlockchainInfo.headers,
+      sizeOnDisk: unrefinedBlockchainInfo.size_on_disk.toString(),
+    };
+  } catch (error) {
+    throw error;
+  }
+};
+
+const getBitcoinBlock = (bitcoinClient, blockHash) => {
+  return new Promise((resolve, reject) => {
+    bitcoinClient.getBlock(blockHash, (err, result) => {
+      if (err) {
+        reject(err);
       } else {
-        try {
-          // Use bestblockhash to call bestBlock, to retrieve time of last block calculation
-          const bestBlockHash = blockchainInfo.bestblockhash
-          bitcoinClient.getBlock(bestBlockHash, (error, block) => {
-            if (error) {
-              reject(error)
-            } else {
-              // Add blockTime to blockchainInfo
-              blockchainInfo.blockTime = block.time;
-              resolve(blockchainInfo)
-            }
-          })
-        } catch (error) {
-          reject(error)
-        }
+        resolve(result);
       }
-    })
-  })
+    });
+  });
+};
 
-  const getNetworkInfoPromise = new Promise((resolve, reject) => {
-    bitcoinClient.cmd('getnetworkinfo', (error, networkInfo) => {
-      if (error) {
-        reject(error)
-      } else {
-        try {
-          resolve(networkInfo)
-        } catch (error) {
-          reject(error)
-        }
-      }
-    })
-  })
+const formatMiningInfo = (unrefinedMiningInfo) => {
+  try {
+    return {
+      difficulty: unrefinedMiningInfo.difficulty,
+      networkhashps: unrefinedMiningInfo.networkhashps,
+    };
+  } catch (error) {
+    throw error;
+  }
+};
 
-  const getConnectionCountPromise = new Promise((resolve, reject) => {
-    bitcoinClient.getConnectionCount((error, connectionCount) => {
-      if (error) {
-        reject(error)
-      } else {
-        try {
-          resolve(connectionCount)
-        } catch (error) {
-          reject(error)
-        }
-      }
-    })
-  })
+const formatPeerInfo = (unrefinedPeerInfo) => {
+  try {
+    return unrefinedPeerInfo.map(({ addr, subver }) => ({ addr, subver }));
+  } catch (error) {
+    throw error;
+  }
+};
 
-  const getMiningInfoPromise = new Promise((resolve, reject) => {
-    bitcoinClient.getMiningInfo((error, miningInfo) => {
-      if (error) {
-        reject(error)
-      } else {
-        try {
-          resolve(miningInfo)
-        } catch (error) {
-          reject(error)
-        }
-      }
-    })
-  })
+const formatNetworkInfo = (unrefinedNetworkInfo) => {
+  try {
+    return {
+      version: unrefinedNetworkInfo.version,
+      subversion: unrefinedNetworkInfo.subversion,
+      localaddresses: unrefinedNetworkInfo.localaddresses,
+    };
+  } catch (error) {
+    throw error;
+  }
+};
 
-  const getPeerInfoPromise = new Promise((resolve, reject) => {
-    bitcoinClient.getPeerInfo((error, peerInfo) => {
-      if (error) {
-        reject(error)
-      } else {
-        try {
-          resolve(peerInfo)
-        } catch (error) {
-          reject(error)
-        }
-      }
-    })
-  })
+const getNodeStats = async (bitcoinClient) => {
+  try {
+    const getBlockchainInfoPromise = promisify(bitcoinClient.getBlockchainInfo).bind(bitcoinClient);
+    const getConnectionCountPromise = promisify(bitcoinClient.getConnectionCount).bind(bitcoinClient);
+    const getMiningInfoPromise = promisify(bitcoinClient.getMiningInfo).bind(bitcoinClient);
+    const getPeerInfoPromise = promisify(bitcoinClient.getPeerInfo).bind(bitcoinClient);
+    const getNetworkInfoPromise = promisify(bitcoinClient.getNetworkInfo).bind(bitcoinClient);
 
-  return Promise.all(
-    [
-      getBlockchainInfoPromise,
-      getConnectionCountPromise,
-      getMiningInfoPromise,
-      getPeerInfoPromise,
-      getNetworkInfoPromise
-    ]
-  )
-}
-
+    return Promise.all([
+      await getBlockchainInfoPromise(),
+      await getConnectionCountPromise(),
+      await getMiningInfoPromise(),
+      await getPeerInfoPromise(),
+      await getNetworkInfoPromise(),
+    ]);
+  } catch (error) {
+    throw error;
+  }
+};
