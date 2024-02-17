66d4f1683aa3d0c1fb0530ab57665035b0d7922a
jstefanop <john@future-apps.net>
2024-02-17 14:52:34 -0500

    Add Automode to backend
    Mode settings are now handled by miner firmware

--- backend/apollo-miner/miner_start.sh
+++ backend/apollo-miner/miner_start.sh
@@ -2,12 +2,13 @@
 
 cd /opt/apolloapi/backend/apollo-miner
 settings=$(cat miner_config)
+mode=$(cat mode)
 
 start_hashboards()
 {
     while [ $1 ];
             do
-            screen -dmS miner ./futurebit-miner -comport $1 -ao_mode 1 $settings
+            screen -dmS miner ./futurebit-miner -comport $1 -ao_mode 1 $settings -powermode $mode
             sleep 1
             shift
     done
@@ -24,7 +25,7 @@ gpio write 0 1
 sleep 30
 #start internal hashboard
 
-screen -dmS miner ./futurebit-miner -comport /dev/ttyS1 -ao_mode 1 $settings
+screen -dmS miner ./futurebit-miner -comport /dev/ttyS1 -ao_mode 1 $settings -powermode $mode
 
 #find and start external hashboards
 
