c07891a5fecd03aaac73ee32f67ad54e5920d27b
jstefanop <john@future-apps.net>
2024-04-11 11:42:33 -0400

    Update ckpool_start.sh

--- backend/ckpool/ckpool_start.sh
+++ backend/ckpool/ckpool_start.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+#clear old log files
+rm /opt/apolloapi/backend/ckpool/logs/ckpool.log
+
 screen -dmS ckpool /opt/apolloapi/backend/ckpool/ckpool -B -c /opt/apolloapi/backend/ckpool/ckpool.conf
 
