7e68cc3c0b86abefd7c37b08ed2c26cc3ce937de
Michele Marcucci <michele.marcucci@simplicissimus.it>
2024-04-15 16:09:35 +0200

    Fix migration

--- migrations/20240415123117_node_extra_options.js
+++ migrations/20240415123117_node_extra_options.js
@@ -1,6 +1,6 @@
 exports.up = function (knex, Promise) {
   return knex.schema.table('settings', function (t) {
-    t.boolean('node_max_connections').defaultTo(32);
+    t.integer('node_max_connections').defaultTo(32);
     t.boolean('node_allow_lan').defaultTo(false);
   });
 };
