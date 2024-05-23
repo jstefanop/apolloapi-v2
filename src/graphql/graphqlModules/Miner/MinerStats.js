f4c3a6862a128aad786c543cf1acac85792eb070
Michele Marcucci <michele@befree.it>
2024-05-23 08:19:11 +0200

    Added comport stats for miner

--- src/graphql/graphqlModules/Miner/MinerStats.js
+++ src/graphql/graphqlModules/Miner/MinerStats.js
@@ -17,6 +17,7 @@ module.exports.typeDefs = `
     uuid: String
     version: String
     date: String
+    comport: String
     statVersion: String
     versions: MinerStatsVersion
     master: MinerStatsMaster
