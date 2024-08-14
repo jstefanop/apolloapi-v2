05dc4521b75a2ab9b8cfe03a957895142a9cd85a
Michele Marcucci <michele@befree.it>
2024-08-14 11:40:59 +0200

    Fix error not handled

--- src/store/api/mcu/mcuAppVersion.js
+++ src/store/api/mcu/mcuAppVersion.js
@@ -1,11 +1,29 @@
-const axios = require('axios')
+const axios = require('axios');
 
 module.exports = ({ define }) => {
-  define('version', async (payload, { knex, errors, utils }) => {
-    const gitAppVersion = await axios.get(`https://raw.githubusercontent.com/jstefanop/apolloui-v2/${(process.env.NODE_ENV === 'development') ? 'dev' : 'main'}/package.json`);
-    const currentAppVersion = (gitAppVersion && gitAppVersion.data) ? gitAppVersion.data.version : null;
-    return process.env.NODE_ENV === 'development' ? currentAppVersion : currentAppVersion;
-  }, {
-    auth: true
-  })
-}
+  define(
+    'version',
+    async (payload, { knex, errors, utils }) => {
+      try {
+        const gitAppVersion = await axios.get(
+          `https://raw.githubusercontent.com/jstefanop/apolloui-v2/${
+            process.env.NODE_ENV === 'development' ? 'dev' : 'main'
+          }/package.json`
+        );
+        const currentAppVersion =
+          gitAppVersion && gitAppVersion.data
+            ? gitAppVersion.data.version
+            : null;
+        return process.env.NODE_ENV === 'development'
+          ? currentAppVersion
+          : currentAppVersion;
+      } catch (e) {
+        console.log(e);
+        return e.toString();
+      }
+    },
+    {
+      auth: true,
+    }
+  );
+};
